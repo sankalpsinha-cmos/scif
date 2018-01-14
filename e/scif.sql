cf8fbb0bffc26eb8869bd31c32e457d1306a1580
Mayank Nagda <nagdamayank05@gmail.com>
2018-01-14 22:21:52 +0530

    Add files via upload

---
+++ e/scif.sql
@@ -0,0 +1,73 @@
+
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(15) NOT NULL auto_increment,
+  `product_code` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_desc` varchar(255) NOT NULL,
+  `price` int(10) NOT NULL,
+  `units` int(5) NOT NULL,
+  `total` int(15) NOT NULL,
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL auto_increment,
+  `product_code` varchar(60) NOT NULL,
+  `product_name` varchar(60) NOT NULL,
+  `product_desc` tinytext NOT NULL,
+  `product_img_name` varchar(60) NOT NULL,
+  `qty` int(5) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `product_code` (`product_code`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `qty`, `price`) VALUES
+(1, 'SCIF1', 'Scale', 'Short description about scale', 'scale.jpg', 26, 5000.00),
+(2, 'SCIF2', 'Vernier Calipers', 'Shoert description about Vernier Calipers', 'vc.JPG', 7, 200.00),
+(3, 'SCIF3', 'Screw Gauge', 'Short description about screw gauge', 'sg.jpg', 34, 1000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `pin` int(6) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `type` varchar(20) NOT NULL default 'user',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `address`, `city`, `pin`, `email`, `password`, `type`) VALUES
+(1, 'Mayank', 'Nagda', 'Milky Way', 'Nebula', 000000, 'mayank@scif.com', '0upone1', 'admin');
